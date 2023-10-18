b14565fb0c50255aebd2940ab16b03238f21db80
BEA LAGAYADA <144906586+BeaLagayada@users.noreply.github.com>
2023-10-18 20:38:28 +0800

    Update Lagayada.py

--- Lagayada.py
+++ Lagayada.py
@@ -7,27 +7,28 @@ print("By: Bea A. Lagayada & Jules Rafael J. Mag-isa")
 # prompt the user to enter a number
 user = eval(input("Enter a number: "))
 
-#prompt the user to enter an option
-answer = eval(input(f"How do you want to convert {user} ? Enter from [1-3]:"))
-
 # Options
-print("**********************************************")
+print("\n**********************************************")
 print("[1] Decimal Number to Binary Number")
 print("[2] Decimal Number to Octal Number")
 print("[3] Decimal Number to Hexadecimal Number")
-print("\n**********************************************")
+print("**********************************************")
+
+#prompt the user to enter an option
+answer = eval(input(f"\nHow do you want to convert {user} ? Enter from [1-3]:"))
 
 # Calculating the number systems
 if answer >= 4:
     print("Invalid Option. Please try again.", answer)
 elif answer == 1:
+    print("\n**********************************************")
     print("Decimal", user, "Converted to Binary Number is", format(user, 'b'))
     print("**********************************************")
 elif answer == 2:
+    print("\n**********************************************")
     print("Decimal", user, "Converted to Binary Number is", format(user, 'o'))
     print("**********************************************")
 else:
+    print("\n**********************************************")
     print("Decimal", user, "Converted to Binary Number is", format(user, 'x'))
     print("**********************************************")
-
-
